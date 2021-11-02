78ddac0c840cd9c37d0dddfe61232415233c63c7
SkolotajsRaivis <71266644+raivis-rasnacs@users.noreply.github.com>
2021-11-02 18:05:34 +0000

    Added variable b1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int saskaitisana(int a, int b) {
-int c = a + b;
+int saskaitisana(int a, int b, int b1) {
+int c = a + b + b1;
 return c;
 }
 
 int main() {
-int a, b, c; 
+int a, b, b1, c; 
 cout << "Ievadi skaitļus!"<<endl;
 cin >> a >> b;
-c = saskaitisana(a, b);
+c = saskaitisana(a, b, b1);
 cout << c;
 }
 
